1294b5cacdcc81a9da0a205d5287da768e480c50
icedream <icedream2k9@die-optimisten.net>
2014-10-23 04:03:20 +0200

    Fix no arguments causing crash.

--- src/addoncreator/Program.cs
+++ src/addoncreator/Program.cs
@@ -10,7 +10,7 @@ namespace GarrysMod.AddonCreator
     {
         private static void Main(string[] args)
         {
-            switch (args[0])
+            switch (args.Length == 0 ? "" : args[0])
             {
                 case "create":
                 {
